479cd2a6c95484e8b90eb3c5873fe917292f72cb
a1623382 <a1623382@users.noreply.github.com>
2026-07-19 22:09:29 +0800

    test: full MainWindow.xaml with minimal Controls to isolate crash

--- src/RoboCopyGUI/Controls/AppTitleBar.xaml.cs
+++ src/RoboCopyGUI/Controls/AppTitleBar.xaml.cs
@@ -1,30 +1,9 @@
-using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 
 namespace RoboCopyGUI.Controls;
 
 public sealed partial class AppTitleBar : UserControl
 {
-    public static readonly DependencyProperty SubtitleProperty =
-        DependencyProperty.Register(nameof(Subtitle), typeof(string), typeof(AppTitleBar),
-            new PropertyMetadata(string.Empty));
-
-    public static readonly DependencyProperty TrailingContentProperty =
-        DependencyProperty.Register(nameof(TrailingContent), typeof(object), typeof(AppTitleBar),
-            new PropertyMetadata(null));
-
-    public string Subtitle
-    {
-        get => (string)GetValue(SubtitleProperty);
-        set => SetValue(SubtitleProperty, value);
-    }
-
-    public object TrailingContent
-    {
-        get => GetValue(TrailingContentProperty);
-        set => SetValue(TrailingContentProperty, value);
-    }
-
     public AppTitleBar()
     {
         InitializeComponent();
